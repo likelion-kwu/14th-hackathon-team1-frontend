aa9c7a31006df0839a561dd8a9242f0098838d5f
EunBin <rhdmsqls1234@naver.com>
2026-08-14 15:26:15 +0900

    [Feat] 온보딩 및 메인 페이지 연결

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,29 @@
+import { useState } from 'react';
 import './App.css'
+import { OnboardingPage } from './pages/OnboardingPage'
+import { MainPage } from './pages/MainPage';
 
 function App() {
+  // 온보딩 완료 핸들러
+  const [isOnboardingCompleted, setIsOnboardingCompleted] = useState(false);
+
+  const [userProfile, setUserProfile] = useState(null);
+
+  const handleOnboardingComplete = (completedData) => {
+    console.log('온보딩 완료 - 저장된 데이터:', completedData);
+    setUserProfile(completedData);
+    setIsOnboardingCompleted(true);
+  };
 
   return (
-    <>
-    </>
+    <div>
+      {!isOnboardingCompleted ? (
+        /* 온보딩 페이지 실행 */
+        <OnboardingPage onComplete={handleOnboardingComplete} />
+      ) : (
+        <MainPage userData={userProfile} />
+      )}
+    </div>
   )
 }
 
